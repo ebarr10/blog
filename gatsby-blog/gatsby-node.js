c91e95035c18a2bcce6fddaaa55505c8fc1505cc
Ethan <ej.barr00@gmail.com>
2025-06-02 19:32:08 -0400

    finalized gatsby website

--- gatsby-blog/gatsby-node.js
+++ gatsby-blog/gatsby-node.js
@@ -4,15 +4,53 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type == `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.nodes.forEach(({ fields }) => {
+      createPage({
+        path: fields.slug,
+        component: require.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: fields.slug,
+        },
+      })
+    })
   })
 }
